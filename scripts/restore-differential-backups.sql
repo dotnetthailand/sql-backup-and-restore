a4f64973c114618236bb9f92e2897bbad9e740f2
aaronamm <theeranitp@gmail.com>
2022-02-18 11:19:17 +0700

    Remove unsed scripts

--- scripts/restore-differential-backups.sql
+++
@@ -1,7 +0,0 @@
-DECLARE @databaseName NVARCHAR(64) = '$(databaseName)'
-DECLARE @backupFilePath NVARCHAR(256) = '$(backupFilePath)'
-
--- Restore most recent differential backup
-RESTORE DATABASE @databaseName
-FROM DISK = @backupFilePath
-WITH RECOVERY; -- make database available and no more backup files can be applied
